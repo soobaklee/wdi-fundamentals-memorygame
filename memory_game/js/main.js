2794e032b7abefdbbd9e9198992f3bc6cdae6d3e
Sunnie <soobak@gmail.com>
2019-01-21 20:44:42 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -21,22 +21,32 @@ var cards = [
 var cardsInPlay = [];
 
 var checkForMatch = function() {
+	this.setAttribute('src', cards[cardId].rank);
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
 	} else {
 		alert("Sorry, try again.")
 	}
 }
-var flipCard = function(cardId) {
-	
+var flipCard = function() {
+	var cardId = this.getAttribute('data-id');
 	console.log("User flipped "+cards[cardId].rank+".");
 	cardsInPlay.push(cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
 }
 
-flipCard(1);
-flipCard(3);
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src','images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click',flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+
+createBoard();
 
 checkForMatch();
 
